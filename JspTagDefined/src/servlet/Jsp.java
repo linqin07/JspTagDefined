aa9a5f49da9ff57d0cc4edb8bcd6b1f4ba4c4554
LinQin <woshilinqin163@163.com>
2018-03-09 15:15:37 +0800

    Test

--- JspTagDefined/src/servlet/Jsp.java
+++ JspTagDefined/src/servlet/Jsp.java
@@ -21,7 +21,7 @@ public class Jsp extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doGet(request, response)  ;
 	}
 
 }
